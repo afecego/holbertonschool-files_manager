9459cba1d0536739be3d33d1aa87505ff90d1542
afecego <afecego@gmail.com>
2022-06-22 23:40:35 -0500

    Fix async

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,6 +5,6 @@ exports.getStatus = (req, res) => {
   res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
 };
 
-exports.getStats = (req, res) => {
-  res.json({ users: dbClient.nbUsers(), files: dbClient.nbFiles() });
+exports.getStats = async (req, res) => {
+  res.status(200).json({ users: await dbClient.nbUsers(), files: await dbClient.nbFiles() });
 };
