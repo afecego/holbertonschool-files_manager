4e3029ee6ac1bd4ed87a01084298b4b19424bcca
afecego <afecego@gmail.com>
2022-06-24 22:33:47 -0500

    fix

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const routes = require('./routes/index');
 
+app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
