d1025b8918e3ef9cfa969279219066a2fa6f1ca7
afecego <afecego@gmail.com>
2022-06-24 22:06:37 -0500

    fix

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -5,21 +5,21 @@ const dbClient = require('../utils/db');
 exports.postNew = async (req, res) => {
   const { email, password } = req.body;
   if (!email) {
-    return res.status(400).send({ error: 'Missing email' });
+    return res.status(400).json({ error: 'Missing email' });
   }
   if (!password) {
-    return res.status(400).send({ error: 'Missing password' });
+    return res.status(400).json({ error: 'Missing password' });
   }
   const emailTrue = await dbClient.user.findOne({ email });
   if (emailTrue) {
-    return res.status(400).send({ error: 'Already exist' });
+    return res.status(400).json({ error: 'Already exist' });
   }
   const sha = sha1(password);
   let insert;
   try {
     insert = await dbClient.user.insertOne({ email, password: sha });
   } catch (err) {
-    return res.status(400).send({ error: err.message });
+    return res.status(400).json({ error: err.message });
   }
 
   const UsQue = Queue('UsQue');
@@ -29,5 +29,5 @@ exports.postNew = async (req, res) => {
     id: insert.insertedId,
     email,
   };
-  return res.status(201).send(result);
+  return res.status(201).json(result);
 };
